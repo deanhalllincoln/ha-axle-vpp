e40a357bb155393344b7b0f6ef8c6b3f90853116
deanhalllincoln <136107184+deanhalllincoln@users.noreply.github.com>
2025-12-04 18:43:16 +0000

    Refactor async_setup_entry and async_unload_entry methods

--- custom_components/axle_vpp/__init__.py
+++ custom_components/axle_vpp/__init__.py
@@ -1,20 +1,18 @@
 from __future__ import annotations
-from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN
-from .coordinator import AxleCoordinator
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    token = entry.data["token"]
-    coordinator = AxleCoordinator(hass, token)
-    await coordinator.async_config_entry_first_refresh()
+PLATFORMS: list[str] = ["sensor"]
 
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Axle VPP from a config entry."""
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    await hass.config_entries.async_unload_platforms(entry, ["sensor"])
-    hass.data[DOMAIN].pop(entry.entry_id)
-    return True
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload Axle VPP config entry."""
+    unloaded = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    return unloaded
