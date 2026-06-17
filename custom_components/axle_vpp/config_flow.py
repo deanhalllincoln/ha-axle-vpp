379622a202b119279f3054cedc98f8a278ea8111
deanhalllincoln <136107184+deanhalllincoln@users.noreply.github.com>
2026-06-17 18:14:36 +0100

    Refactor async_step_user and add async_step_reconfigure

--- custom_components/axle_vpp/config_flow.py
+++ custom_components/axle_vpp/config_flow.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
+
 import voluptuous as vol
+
 from homeassistant import config_entries
 from homeassistant.const import CONF_TOKEN
 from homeassistant.data_entry_flow import FlowResult
 
 from .const import DOMAIN
+
 
 class AxleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -20,4 +23,23 @@ class AxleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             vol.Required(CONF_TOKEN): str
         })
 
-        return self.async_show_form(step_id="user", data_schema=schema)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema,
+        )
+
+    async def async_step_reconfigure(self, user_input=None) -> FlowResult:
+        if user_input is not None:
+            return self.async_update_reload_and_abort(
+                self._get_reconfigure_entry(),
+                data_updates={CONF_TOKEN: user_input[CONF_TOKEN]},
+            )
+
+        schema = vol.Schema({
+            vol.Required(CONF_TOKEN): str
+        })
+
+        return self.async_show_form(
+            step_id="reconfigure",
+            data_schema=schema,
+        )
